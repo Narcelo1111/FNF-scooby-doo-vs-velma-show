65f8d9496022492731913afa4c04ce630a41e432
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2023-02-24 11:56:03 -0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2238,7 +2238,7 @@ class PlayState extends MusicBeatState
 	public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ ' | Misses: ' + songMisses
+		'Port by Guislakkkk' + ' | Misses: ' + songMisses
 		+ ' | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
